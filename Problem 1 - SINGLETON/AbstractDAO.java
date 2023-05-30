9c94bb20e46d5f255430b6bc14c40405066bfc6c
Roxana-Dobrica <100201490+Roxana-Dobrica@users.noreply.github.com>
2023-05-30 19:14:35 +0300

    Singleton Pattern Problem

---
+++ Problem 1 - SINGLETON/AbstractDAO.java
@@ -0,0 +1,13 @@
+package singletonProblem;
+
+import java.util.List;
+
+/**
+ *
+ * @author Roxana Dobrica
+ */
+public abstract class AbstractDAO {
+    public abstract void create() ;
+
+    public abstract List<Superclass> findAll();
+}
